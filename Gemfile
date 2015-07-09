cb9845f847d3f7915c13ce5d4a1c5e314a3d9675
Simon Buysse <buysse.simon@gmail.com>
2015-07-09 10:58:37 +0200

    add lenght and presence validation to todo list title, and wrote a few tests

--- Gemfile
+++ Gemfile
@@ -24,3 +24,6 @@ group :development, :test do
     gem 'rspec-rails', '~> 2.0'
 end
 
+group :test do 
+   gem 'capybara','~>2.1.0' 
+end
---
+++ spec/features/todo-lists/create_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper.rb"
+
+describe "Creating ToDo lists" do 
+    it "redirects to todo list index page on succes" do
+        visit "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New Todo List")
+        
+        fill_in "Title", with: "My Todo list"
+        fill_in "Description", with: "This is what I'm doing today"
+        click_button "Create Todo list"
+        expect(page).to have_content("My Todo list")
+    end
+    it "gives an error when we fill in no name" do
+        expect(TodoList.count).to eq(0)
+
+        visit "/todo_lists"
+        click_link "New Todo list"
+        fill_in "Description", with: "This is what I'm doing today"
+        click_button "Create Todo list"
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+
+        visit "/todo_lists"
+        expect(page).to_not have_content("This is what I'm doing today") 
+    end
+    it "gives an error when the title is less than 3 characters" do
+        expect(TodoList.count).to eq(0)
+        visit "/todo_lists"
+        click_link "New Todo list"
+        fill_in "Title", with: "My"
+        fill_in "Description", with: "This is what I'm doing today"
+        click_button "Create Todo list"
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+
+        visit "/todo_lists"
+        expect(page).to_not have_content("This is what I'm doing today") 
+    end
+
+end
