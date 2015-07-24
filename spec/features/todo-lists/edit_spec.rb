1fbae4c945805afd16627d37360f02be623253ba
Simon Buysse <buysse.simon@gmail.com>
2015-07-24 11:42:33 +0200

    added edit tests

--- spec/features/todo-lists/edit_spec.rb
+++ spec/features/todo-lists/edit_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 describe "Editing to do lists" do 
+        let!(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery list.")}
     def update_todo_list(options={})
         options[:title] ||= "New title"
         options[:description] ||= "New description"
@@ -20,12 +21,30 @@ describe "Editing to do lists" do
     end
 
     it "updates todo list correctly with correct information" do
-        todo_list = TodoList.create(title: "Groceries", description: "Grocery list.")
         update_todo_list todo_list: todo_list
 
         expect(page).to have_content("Todo list was successfully updated.")
         expect(todo_list.title).to eq("New title")
         expect(todo_list.description).to eq("New description")
     end
+
+    it "displays an error with no title" do
+        update_todo_list todo_list: todo_list, title: ""
+
+        expect(page).to have_content("error")
+    end
+    
+    it "displays an error with no description" do
+        update_todo_list todo_list: todo_list, description: ""
+
+        expect(page).to have_content("error")
+    end
+    
+    it "displays an error with too short description" do
+        update_todo_list todo_list: todo_list, description: "Hi"
+
+        expect(page).to have_content("error")
+    end
+
 end
     
