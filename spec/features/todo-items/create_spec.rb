4040e017b420a86b131a0f6bb46404de729bbe14
Simon Buysse <buysse.simon@gmail.com>
2015-07-26 14:52:56 +0200

    added flash error and succes messages, with some css

--- spec/features/todo-items/create_spec.rb
+++ spec/features/todo-items/create_spec.rb
@@ -8,14 +8,37 @@ describe "Adding todo items" do
             click_link "Show items"
         end
     end
+
+    def create_todo_item(options={})
+        options[:content]  ||= "Milk"
+
+        click_link "New todo item"
+        fill_in "Content", with: options[:content]
+        click_button "Save"
+    end
     
     it "is successful with valid content" do
         visit_todo_list(todo_list)
-        click_link "New todo item"
-        fill_in "Content", with: "Milk"
-        click_button "Save"
+        create_todo_item
 
         expect(page).to have_content("Added todo list item.")
         expect(page).to have_content("Milk")
     end
+    it "displays an error when nothing is filled in" do
+        visit_todo_list(todo_list)
+        create_todo_item content: ""
+
+        expect(page).to have_content("There was a problem adding that todo item to the list")
+        expect(page).to have_content("Content can't be blank")
+    end
+    
+    it "displays an error when item is less than 2 characters" do
+        visit_todo_list(todo_list)
+        create_todo_item content: "s"
+
+        expect(page).to have_content("There was a problem adding that todo item to the list")
+        expect(page).to have_content("Content is too short")
+    end
+
+
 end
