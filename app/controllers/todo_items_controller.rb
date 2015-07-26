01044d855e26845f0e946b7277913195c0c46e10
Simon Buysse <buysse.simon@gmail.com>
2015-07-26 17:31:10 +0200

    added specs and code for deleting todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,19 +1,19 @@
 class TodoItemsController < ApplicationController
+
+  before_action :find_todo_list
+
   def index
-      @todo_list = TodoList.find(params[:todo_list_id])
   end
   
   def new
-      @todo_list = TodoList.find(params[:todo_list_id])
       @todo_item = @todo_list.todo_items.new
   end
   
   def create
-     @todo_list = TodoList.find(params[:todo_list_id])
      @todo_item = @todo_list.todo_items.new(todo_item_params)
 
      if @todo_item.save
-         flash[:succes] = "Added todo list item."
+         flash[:success] = "Added todo list item."
          redirect_to(todo_list_todo_items_path(@todo_list))
      else
          flash[:error] = "There was a problem adding that todo item to the list"
@@ -22,16 +22,14 @@ class TodoItemsController < ApplicationController
   end
     
   def edit
-     @todo_list = TodoList.find(params[:todo_list_id])
      @todo_item = @todo_list.todo_items.find(params[:id])
   end
   
   def update
-     @todo_list = TodoList.find(params[:todo_list_id])
      @todo_item = @todo_list.todo_items.find(params[:id])
         
      if @todo_item.update(todo_item_params)
-         flash[:succes]="Saved todo list item."
+         flash[:success]="Saved todo list item."
          redirect_to(todo_list_todo_items_path(@todo_list))
      else
         flash[:error]="Error saving todo item."
@@ -40,8 +38,22 @@ class TodoItemsController < ApplicationController
      
   end
 
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+        flash[:success] = "Succesfully deleted todo item"
+    else
+        flash[:error]="Could not delete todo item"
+    end
+        redirect_to(todo_list_todo_items_path(@todo_list))
+  end
+
   private
   def todo_item_params
     params[:todo_item].permit(:content)
+  end
+
+  def find_todo_list
+     @todo_list = TodoList.find(params[:todo_list_id])
   end
 end
---
+++ spec/features/todo-items/destroy_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper.rb"
+
+describe "Deleting todo_item" do
+    let!(:todo_list) {TodoList.create(title: "Grocery list", description: "Groceries")}
+    let!(:todo_item) {todo_list.todo_items.create(content: "Milk")}
+
+    def visit_todo_list(list)
+        visit "/todo_lists"
+        within "#todo_list_#{list.id}" do
+            click_link "Show items"
+        end
+    end
+
+    def delete_todo_item
+        within("#todo_item_#{todo_item.id}") do
+            click_link "Delete"
+        end
+    end
+ 
+    it "succesfully deletes todo item" do
+        visit_todo_list(todo_list)
+        delete_todo_item
+
+        expect(page).to have_content("Succesfully deleted todo item")
+        expect(TodoItem.count).to eq(0)
+    end
+    
+end
